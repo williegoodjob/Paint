a7f372519b4fe171aecdec2717982771bcc5f2c4
willie <willie@goodjob.idv.tw>
2024-11-13 10:19:21 +0800

    預設選中線條工具並新增動態圖形繪製邏輯
    在 `MainWindow.xaml` 中，將 `line` 的 `RadioButton` 預設設為選中狀態 (`IsChecked="True"`)。
    在 `MainWindow.xaml.cs` 中，新增 `fillColor` 變數來儲存填充顏色，並初始化 `fillColorPicker` 的選中顏色。
    在 `myCanvas_MouseLeftButtonDown` 事件中，新增根據選中形狀類型創建相應圖形的邏輯。
    在 `myCanvas_MouseMove` 事件中，新增根據滑鼠移動動態更新圖形大小和位置的邏輯。
    在 `myCanvas_MouseLeftButtonUp` 事件中，移除滑鼠按鍵釋放時創建線條的邏輯，改為將 `isDrawing` 設為 `false` 來結束繪圖。

--- Paint/MainWindow.xaml.cs
+++ Paint/MainWindow.xaml.cs
@@ -11,9 +11,10 @@ namespace Paint
         Point start = new Point { X = 0, Y = 0 };
         Point dest = new Point { X = 0, Y = 0 };
         Color strokeColor = Colors.Red;
+        Color fillColor = Colors.Red;
         int strokeThickness = 1;
         string shapeType = "";
-
+        bool isDrawing = false;
         private void myCanvas_MouseEnter(object sender, MouseEventArgs e)
         {
             myCanvas.Cursor = Cursors.Pen;
@@ -23,6 +24,54 @@ namespace Paint
         {
             myCanvas.Cursor = Cursors.Cross;
             start = e.GetPosition(myCanvas);
+
+            strokeColor = (Color)strokeColorPicker.SelectedColor;
+            fillColor = (Color)fillColorPicker.SelectedColor;
+
+            Brush brush = new SolidColorBrush(strokeColor);
+            Brush fill = new SolidColorBrush(fillColor);
+
+            switch (shapeType)
+            {
+                case "line":
+                    Line line = new Line
+                    {
+                        Stroke = brush,
+                        StrokeThickness = strokeThickness,
+                        X1 = start.X,
+                        Y1 = start.Y,
+                        X2 = dest.X,
+                        Y2 = dest.Y
+                    };
+                    myCanvas.Children.Add(line);
+                    break;
+                case "rectangle":
+                    Rectangle rectangle = new Rectangle
+                    {
+                        Stroke = brush,
+                        StrokeThickness = strokeThickness,
+                        Width = 0,
+                        Height = 0,
+                        Fill = fill
+                    };
+                    myCanvas.Children.Add(rectangle);
+                    break;
+                case "ellipse":
+                    Ellipse ellipse = new Ellipse
+                    {
+                        Stroke = brush,
+                        StrokeThickness = strokeThickness,
+                        Width = 0,
+                        Height = 0,
+                        Fill = fill
+                    };
+                    myCanvas.Children.Add(ellipse);
+                    break;
+                case "polyline":
+                    break;
+            }
+            isDrawing = true;
+
             DisplayStatus(start, dest);
         }
 
@@ -35,28 +84,64 @@ namespace Paint
         {
             InitializeComponent();
             strokeColorPicker.SelectedColor = strokeColor;
+            fillColorPicker.SelectedColor = fillColor;
         }
 
         private void myCanvas_MouseMove(object sender, MouseEventArgs e)
         {
             dest = e.GetPosition(myCanvas);
+            if (isDrawing == true)
+            {
+                Point origin;
+                double sizeW, sizeH;
+                origin.X = Math.Min(start.X, dest.X);
+                origin.Y = Math.Min(start.Y, dest.Y);
+                sizeW = Math.Abs(start.X - dest.X);
+                sizeH = Math.Abs(start.Y - dest.Y);
+
+                switch (shapeType)
+                {
+                    case "line":
+                        var line = myCanvas.Children.OfType<Line>().LastOrDefault();
+                        line.X2 = dest.X;
+                        line.Y2 = dest.Y;
+                        break;
+                    case "rectangle":
+                        var rectangle = myCanvas.Children.OfType<Rectangle>().LastOrDefault();
+                        rectangle.Width = sizeW;
+                        rectangle.Height = sizeH;
+                        rectangle.SetValue(Canvas.LeftProperty, origin.X);
+                        rectangle.SetValue(Canvas.TopProperty, origin.Y);
+                        break;
+                    case "ellipse":
+                        var ellipse = myCanvas.Children.OfType<Ellipse>().LastOrDefault();
+                        ellipse.Width = sizeW;
+                        ellipse.Height = sizeH;
+                        ellipse.SetValue(Canvas.LeftProperty, origin.X);
+                        ellipse.SetValue(Canvas.TopProperty, origin.Y);
+                        break;
+                    case "polyline":
+                        break;
+                }
+            }
             DisplayStatus(start, dest);
         }
 
         private void myCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            strokeColor = (Color)strokeColorPicker.SelectedColor;
-            Brush brush = new SolidColorBrush(strokeColor);
-            Line line = new Line
-            {
-                Stroke = brush,
-                StrokeThickness = strokeThickness,
-                X1 = start.X,
-                Y1 = start.Y,
-                X2 = dest.X,
-                Y2 = dest.Y
-            };
-            myCanvas.Children.Add(line);
+            //strokeColor = (Color)strokeColorPicker.SelectedColor;
+            //Brush brush = new SolidColorBrush(strokeColor);
+            //Line line = new Line
+            //{
+            //    Stroke = brush,
+            //    StrokeThickness = strokeThickness,
+            //    X1 = start.X,
+            //    Y1 = start.Y,
+            //    X2 = dest.X,
+            //    Y2 = dest.Y
+            //};
+            //myCanvas.Children.Add(line);
+            isDrawing = false;
         }
 
         private void strokeThicknessSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
